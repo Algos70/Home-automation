3353f9b6ede84200eb1935b70c1fe5916b5324c7
Kursat <draknes.gamin70@gmail.com>
2023-04-19 16:34:23 +0300

    Deleted database connection from login.php

--- login.php
+++ login.php
@@ -1,60 +1,24 @@
 <?php
-$conn = mysqli_connect('localhost', 'algos', '123456', 'dbtest');
-
-//write query for all infos
-$sql = 'SELECT email, pw, isAdmin FROM pinfo';
-
-//make query and get result
-$result = mysqli_query($conn, $sql);
-
-//fetch the resulting rows as an array
-$infos = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-//frees result from memory for good practice
-mysqli_free_result($result);
-
-//close connection if you want to
-//mysqli_close($conn);
-
-//print_r($infos);
-
-
+//if login button pressed take inputs and compare them to find successful login
 if (isset($_POST['login'])) {
     //secure the input and get them from the global variable post
     $email = filter_var($_POST['email'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $password = filter_var($_POST['password'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-    $i = 0;
-    $isEmailFound = false;
 
-    foreach ($infos as $item) {
-        if ($item['email'] === $email) {
-            $isEmailFound = true;
-            break;
-        }
-        $i++;
-    }
+    $producerEmail = 'b@gmail.com';
+    $producerPw = '123';
+    $customerEmail = 'a@gmail.com';
+    $customerPw = '1234';
 
-
-    //If input is in the database then get them from the data base
-    //else just show wrong password to the user like the user input the 
-    //wrong password
-    if ($isEmailFound) {
-        //variables from the database
-        $email = $infos[$i]['email'];
-        $pw = $infos[$i]['pw'];
-        $isAdmin = $infos[$i]['isAdmin'];
-
-        if ($email === $email  && $password === $pw && $isAdmin == 0) {
-            echo "Zort not an admin";
-            //redirect to the customer
-            header("Location: ./customer.php");
-        } else if (($email === $email  && $password === $pw && $isAdmin == 1)) {
-            echo 'Zort admin';
-            //redirect to producer
-            header("Location: ./producer.php");
-        } else {
-        }
+    if ($email === $customerEmail && $password === $customerPw) {
+        echo "Zort not an admin";
+        //redirect to the customer
+        header("Location: ./customer.php");
+    } else if (($email === $producerEmail && $password === $producerPw)) {
+        echo 'Zort admin';
+        //redirect to producer
+        header("Location: ./producer.php");
     } else {
     }
 }
